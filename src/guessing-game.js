e64d46e4562dc0233e8d348c555449eca0a4b01d
Дмитрий Якорев <urakend@mail.ru>
2019-10-11 11:57:24 +0300

    success

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guess1=0;
+        this.min1=0;
+        this.max1=0;
+    }
 
     setRange(min, max) {
-
+        this.min1=min;
+         this.max1=max;
     }
 
     guess() {
-
+       this.guess1=Math.round((this.max1+this.min1)/2);
+       
+        console.log("guess",this.guess1);
+        return this.guess1;
     }
 
     lower() {
-
+        this.max1=this.guess1;
     }
 
     greater() {
-
+        this.min1=this.guess1;
     }
 }
 
